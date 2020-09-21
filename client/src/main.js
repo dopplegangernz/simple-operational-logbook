85d9e71472e03fc626e1de00bd9284c1bbd0edbd
David Maclagan <dmaclagan@teamtalk.co.nz>
2020-09-21 19:09:46 +1200

    Adds vuex

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,34 @@
 import Vue from "vue";
 import App from "./App.vue";
 import VCalendar from "v-calendar";
+import Vuex from "vuex";
 
 Vue.config.productionTip = false;
-
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
 
 Vue.use(VCalendar, {
   componentPrefix: "vc",
 });
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    user: {
+      username: null,
+      email: null,
+      group: null,
+      id: null,
+      authKey: null,
+      admin: false,
+    },
+    date: null,
+    searchString: null,
+    entries: [],
+  },
+  mutations: {},
+});
+
+new Vue({
+  store: store,
+  render: (h) => h(App),
+}).$mount("#app");
