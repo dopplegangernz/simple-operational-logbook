20a3fb237b5450c9d8bd183a299017bcdb5faae6
David Maclagan <morat@Oracle.local>
2020-09-26 13:37:31 +1200

    Moves the base URL of the api from / to /api

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask_restx import Api
 from flask import Blueprint
 
+
 from .main.controller.user_controller import api as user_ns
 from .main.controller.group_controller import api as group_ns
 from .main.controller.auth_controller import api as auth_ns
@@ -10,10 +11,12 @@ from .main.controller.logEntries_controller import api as logEntries_ns
 blueprint = Blueprint('api', __name__)
 
 api = Api(blueprint,
-          title='FLASK RESTPLUS(RESTX) API BOILER-PLATE WITH JWT',
-          version='1.0',
-          description='a boilerplate for flask restplus (restx) web service'
+          title='Simple Operational Logbook',
+          version='0.1',
+          description='A simple logbook for recording operational activities',
+          doc='/api/'
           )
+
 
 api.add_namespace(group_ns, path='/group')
 api.add_namespace(user_ns, path='/user')
--- client/src/vuex/actions.js
+++ client/src/vuex/actions.js
@@ -1,7 +1,7 @@
 const axios = require("axios").default;
 
 const axiosConfig = {
-  baseURL: "http://localhost:5000",
+  baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
