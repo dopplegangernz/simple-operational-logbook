35cd00b9fbc25649c73a7406a7fabe62eb77b703
David Maclagan <morat@Oracle.local>
2020-09-26 13:50:12 +1200

    Serves client files from /

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+import os
 
 from .config import config_by_name
 
@@ -10,7 +11,8 @@ flask_bcrypt = Bcrypt()
 
 
 def create_app(config_name):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder=os.path.join(
+        os.path.dirname(__file__), '../../client/dist/'), static_url_path='')
     CORS(app)
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
